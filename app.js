1221f742e3429553bdf62b1e5584f04bd4341ddd
user <bjohnso@student.wethinkcode.co.za>
2019-05-27 15:47:51 +0200

    Added handlebars, views, sessions, and MongoDB persitance

--- app.js
+++ app.js
@@ -3,15 +3,27 @@ const express = require('express');
 const database = require('./config/database');
 const passport = require('passport');
 const keys = require('./config/keys');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const exphbs = require('express-handlebars');
 
 const app = express();
 
 //Load Routes
 
+const index = require('./routes/index');
 const users = require('./routes/users');
 const auth = require('./routes/auth');
 
 //Load Middleware
+
+    //Express-Session
+    app.use(session({
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true,
+        store: new MongoStore({ mongooseConnection: mongoose.connection})
+    }));
 
     //Passport
     app.use(passport.initialize());
@@ -25,13 +37,27 @@ const auth = require('./routes/auth');
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
 
-//Routes
+    //Global Variables
+    app.use((req, res, next) => {
+        //res.locals.success_msg = req.flash('success_msg');
+        //res.locals.error_msg = req.flash('error_msg');
+        //res.locals.error = req.flash('error');
+        res.locals.user = req.user || null;
+        next();
+    });
 
-app.get('/', (req, res) => {
-    res.send('Awe');
-});
+    //Handlebars
+    app.engine('handlebars', exphbs({
+        defaultLayout: 'main'
+    }));
+    app.set('view engine', 'handlebars');
 
+//Enable Routes
+app.use('/', index);
+app.use('/users', users);
 app.use('/auth', auth);
+
+//Port and Server
 
 const port = process.env.PORT || 5000;
 
